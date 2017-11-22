16111ce1822be412bcdc78005ee5a1e42a7997e4
Greg Pfaff <greg.pfaff@razorfish.com>
2017-11-22 12:25:54 -0600

    adding updates to store picker

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,19 @@
 // let's go!
+
+import React, { Component } from 'react';
+import { render } from "react-dom";
+
+import StorePicker from './components/StorePicker';
+
+import './css/style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <StorePicker />
+    )
+  }
+}
+
+render(<App />, document.querySelector('#main'));
+
